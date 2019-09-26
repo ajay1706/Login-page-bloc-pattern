8b8acbbcaeb8be05674600110d291ba7dc5547c7
ajay1706 <ajay97456@gmail.com>
2019-09-26 16:20:33 +0530

    adding rxdart latest2combine

--- login_bloc/lib/lib/bloc.dart
+++ login_bloc/lib/lib/bloc.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:login_bloc/lib/validator.dart';
-
+import 'package:rxdart/rxdart.dart';
 class Bloc extends Object with Validators implements BaseBloc{
 final _emailController = StreamController<String>();
 final _passwordController = StreamController<String>();
@@ -10,6 +10,7 @@ final _passwordController = StreamController<String>();
 Stream<String> get email=> _emailController.stream.transform(emailValidator);
 Stream<String> get password=> _emailController.stream.transform(passwordValidator);
 
+Stream<bool> get submitCheck => Observable.combineLatest2(email, password, (e,p)=> true);
 
 @override
   void dispose() {
