e7f480879508a6af407a30f8114108a908976ddc
ajay1706 <ajay97456@gmail.com>
2019-09-26 16:16:07 +0530

    adding password validators

--- login_bloc/lib/lib/bloc.dart
+++ login_bloc/lib/lib/bloc.dart
@@ -1,8 +1,15 @@
 import 'dart:async';
 
-class Bloc implements BaseBloc{
+import 'package:login_bloc/lib/validator.dart';
+
+class Bloc extends Object with Validators implements BaseBloc{
 final _emailController = StreamController<String>();
 final _passwordController = StreamController<String>();
+
+
+Stream<String> get email=> _emailController.stream.transform(emailValidator);
+Stream<String> get password=> _emailController.stream.transform(passwordValidator);
+
 
 @override
   void dispose() {
