ecbc12fe4f96edabc118720b022bbe4b217b83d6
ajay1706 <ajay97456@gmail.com>
2019-09-26 15:42:41 +0530

    adding files and page UI

---
+++ login_bloc/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(  
+        primarySwatch: Colors.cyan
+      ),
+
+      
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.all(16),
+                height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisSize:MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Enter Email",
+                  labelText: "Email"  
+
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+               TextField(
+                 obscureText: true,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Enter Password",
+                  labelText: "Password"  
+
+                ),
+              ),
+                SizedBox(
+                height: 20,
+              ),
+              RaisedButton(
+                child: Text("Submit"),
+                onPressed: (){},
+                color: Colors.cyan,
+              )
+
+            ],
+          ),
+        ),
+      ),
+      
+    );
+  }
+}
